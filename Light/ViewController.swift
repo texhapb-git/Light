8b659b99f9904cdc4a7133ded6fbd0cf87641165
TexHaPb <texhapb@bk.ru>
2020-10-23 23:23:00 +0300

    Added real flashlight

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -6,10 +6,13 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     var isLightOn: Bool = true
+    
+    
     
     override var prefersStatusBarHidden: Bool{
         return true
@@ -24,6 +27,27 @@ class ViewController: UIViewController {
 
     fileprivate func updateView() {
         view.backgroundColor = isLightOn ? .white : .black
+        
+        guard let device = AVCaptureDevice.default(for: AVMediaType.video) else { return }
+        guard device.hasTorch else { return }
+
+        do {
+            try device.lockForConfiguration()
+
+            if (device.torchMode == AVCaptureDevice.TorchMode.on) {
+                device.torchMode = AVCaptureDevice.TorchMode.off
+            } else {
+                do {
+                    try device.setTorchModeOn(level: 1.0)
+                } catch {
+                    print(error)
+                }
+            }
+
+            device.unlockForConfiguration()
+        } catch {
+            print(error)
+        }
     }
     
     @IBAction func buttonPressed() {
